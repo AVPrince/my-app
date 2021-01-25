ee5779fbf0e078e03d5d87e95dfcf1397d93480d
Colliding Pictures <macuser@Alans-iMac-2.local>
2021-01-25 14:45:44 -0600

    added routing to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
+
+import Navigation from "./components/Navigation/Navigation";
 import Welcome from './components/Welcome/Welcome'
 import Clock from './components/Clock/Clock'
 import Contact from "./components/Contact/Contact";
 
+
 function App() {
   return (
     <div className="App">
-      <Welcome name="Alan" />
-      <Clock />
-      <Contact />
+      <Navigation />
+      <Route path="/" exact component={Welcome}/>
+      <Route path ="/clock" component={Clock} />
+      <Route path ="/contact" component={Contact} />
     </div>
   );
 }
