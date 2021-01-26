9c4b0fc28cb739d81d7b8f8f11e947b26e0b6b58
Colliding Pictures <macuser@Alans-iMac-2.local>
2021-01-26 15:58:35 -0600

    added more routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import Navigation from "./components/Navigation/Navigation";
 import Welcome from './components/Welcome/Welcome'
 import Clock from './components/Clock/Clock'
 import Contact from "./components/Contact/Contact";
+import $404 from "./components/404/404";
 
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Route 
-      path="/" 
-      exact 
-      render={(...props) => <Welcome name="Alan" />} />
-      <Route path ="/clock" component={Clock} />
-      <Route path ="/contact" component={Contact} />
+      <Switch>
+        <Route 
+        path="/" 
+        exact 
+        render={(props) => <Welcome {...props} name="Alan" />} />
+        <Route path ="/clock" component={Clock} />
+        <Route path ="/contact" component={Contact} />
+        <Route path="/welcome/:name" component={Welcome} />
+        <Route path="/404" component={$404} />
+      </Switch>
     </div>
   );
 }
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function Welcome (props){
+    console.log(props.match);
+    console.log(props.name);
+    let name = props.match && props.match.params.name || props.name || "Default";
     return (
         <div className="Welcome">
-            Welcome, {props.name}!
+            Welcome, {name}!
         </div>
-    )
+    );
 }
 
 export default Welcome;
